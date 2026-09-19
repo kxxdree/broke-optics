fc31df31bbdcdf3c3d42ec040be062b5a767c243
kxxdree <truebackward@gmail.com>
2026-09-19 16:48:21 +0300

    feat: wip how it works section

--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -4,14 +4,14 @@ import TelegramIcon from "~/assets/icons/telegram-icon.svg?component";
 </script>
 
 <template>
-  <section class="relative w-full overflow-hidden py-12">
+  <section class="relative w-full overflow-hidden min-h-screen py-12">
     <!-- Фоновая заглушка для очков (вставь сюда картинку через bg-[url(...)]) -->
     <div class="absolute inset-0 z-0 pointer-events-none"></div>
 
-    <div class="relative z-10 grow flex flex-col justify-center max-w-[1920px] mx-auto px-6 w-full py-12">
-      <h1 class="text-white/70 text-lg tracking-[0.45em] uppercase mb-6 font-light">Оптика нового формата</h1>
+    <div class="relative z-10 grow flex flex-col justify-center max-w-[1920px] mx-auto px-28 w-full mb-12">
+      <h1 class="text-[#c19d60] text-md tracking-[0.45em] uppercase mb-4 font-light">Оптика нового формата</h1>
 
-      <h2 class="text-5xl md:text-[4rem] leading-tight uppercase mb-6 max-w-2xl font-light tracking-widest">
+      <h2 class="text-5xl md:text-[5rem] leading-[1.1] uppercase mb-6 font-light tracking-wide">
         Меняем линзы<br />
         в вашей оправе<br />
         без переплат
@@ -19,17 +19,15 @@ import TelegramIcon from "~/assets/icons/telegram-icon.svg?component";
 
       <div class="w-16 h-px bg-[#c19d60] mb-6"></div>
 
-      <p class="text-gray-300 text-lg leading-normal max-w-md mb-6">
-        Изготавливаем линзы по вашему рецепту<br />
-        для любых оправ — быстро, точно<br />
-        и дешевле сетевых оптик
+      <p class="text-gray-300 text-lg leading-normal max-w-xl mb-6">
+        Изготавливаем линзы по вашему рецепту для любых оправ — быстро, точно и дешевле сетевых оптик
       </p>
 
-      <div class="flex items-center flex-wrap gap-4 text-[0.85rem] text-white/60 uppercase tracking-widest mb-12">
+      <div class="flex items-center flex-wrap gap-4 text-[0.75rem] text-white/60 uppercase tracking-widest mb-12">
         <span>Прозрачные</span>
-        <span class="text-[#c19d60] text-[8px]">●</span>
+        <span class="text-[#c19d60] text-[0.5rem]">●</span>
         <span>Фотохромные</span>
-        <span class="text-[#c19d60] text-[8px]">●</span>
+        <span class="text-[#c19d60] text-[0.5rem]">●</span>
         <span>Тонированные</span>
       </div>
 
@@ -43,26 +41,26 @@ import TelegramIcon from "~/assets/icons/telegram-icon.svg?component";
       </div>
     </div>
 
-    <ul class="relative z-10 w-full mx-auto px-6 pb-12 mt-auto">
+    <ul class="relative z-10 w-full max-w-[1920px] mx-auto px-28 mt-auto">
       <div class="flex items-start gap-12">
         <li class="flex flex-col border-l border-[#c19d60] pl-4">
-          <span class="text-white/70 text-sm uppercase mb-1 tracking-wide">100%</span>
-          <span class="text-white/60 text-[0.575rem] uppercase tracking-widest">Оригинальные линзы</span>
+          <span class="text-white/90 text-lg uppercase mb-1 tracking-wide">100%</span>
+          <span class="text-white/60 text-sm uppercase tracking-widest">Оригинальные линзы</span>
         </li>
 
         <li class="flex flex-col border-l border-[#c19d60] pl-4">
-          <span class="text-white/70 text-sm uppercase mb-1 mt-1 tracking-wide">Гарантия</span>
-          <span class="text-white/60 text-[0.575rem] uppercase tracking-widest">На все работы</span>
+          <span class="text-white/90 text-lg uppercase mb-1 mt-1 tracking-wide">Гарантия</span>
+          <span class="text-white/60 text-sm uppercase tracking-widest">На все работы</span>
         </li>
 
         <li class="flex flex-col border-l border-[#c19d60] pl-4">
-          <span class="text-white/70 text-sm uppercase mb-1 mt-1 tracking-wide">Быстрое</span>
-          <span class="text-white/60 text-[0.575rem] uppercase tracking-widest">Изготовление</span>
+          <span class="text-white/90 text-lg uppercase mb-1 mt-1 tracking-wide">Быстрое</span>
+          <span class="text-white/60 text-sm uppercase tracking-widest">Изготовление</span>
         </li>
 
         <li class="flex flex-col border-l border-[#c19d60] pl-4">
-          <span class="text-white/70 text-sm uppercase mb-1 mt-1 tracking-wide">Доставка</span>
-          <span class="text-white/60 text-[0.575rem] uppercase tracking-widest">По всей Москве и России</span>
+          <span class="text-white/90 text-lg uppercase mb-1 mt-1 tracking-wide">Доставка</span>
+          <span class="text-white/60 text-sm uppercase tracking-widest">По всей Москве и России</span>
         </li>
       </div>
     </ul>
--- app/components/LensesSection.vue
+++ app/components/LensesSection.vue
@@ -28,11 +28,11 @@ const cards: { num: string; title: string; desc: string; image: string }[] = [
 </script>
 
 <template>
-  <section class="relative w-full overflow-hidden pb-12">
+  <section class="relative w-full overflow-hidden py-12">
     <!-- Заглушка для фонового изображения секции. Замени на свой bg-[url('...')] -->
     <div class="absolute inset-0 z-0 pointer-events-none"></div>
 
-    <div class="max-w-[1920px] mx-auto px-6 relative z-10">
+    <div class="max-w-[1920px] mx-auto px-28 relative z-10">
       <div class="mb-12">
         <h2 class="text-5xl md:text-[4rem] leading-tight uppercase mb-6 font-light tracking-widest">
           Изготавливаем<br />
