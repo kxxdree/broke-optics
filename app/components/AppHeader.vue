abb0e71730a654839554517594dbb337c20f4e88
kxxdree <truebackward@gmail.com>
2026-09-19 13:57:44 +0300

    feat: wip cheaper section

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import BaseButton from "./UI/BaseButton.vue";
+import TelegramIcon from "~/assets/icons/telegram-icon.svg?component";
+import InstagramIcon from "~/assets/icons/instagram-icon.svg?component";
+import AvitoIcon from "~/assets/icons/avito-icon.svg?component";
 </script>
 
 <template>
@@ -16,18 +19,14 @@ import BaseButton from "./UI/BaseButton.vue";
       <div class="flex items-center gap-10">
         <div class="flex items-center gap-6 text-gray-400">
           <a href="#" class="hover:text-white transition-colors">
-            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-              <path
-                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.64 6.8c-.15 1.58-.8 5.42-1.13 7.19-.14.75-.42 1-.68 1.03-.58.05-1.02-.38-1.58-.75-.88-.58-1.38-.94-2.23-1.5-.99-.65-.35-1.01.22-1.59.15-.15 2.71-2.48 2.76-2.69a.2.2 0 00-.05-.18c-.06-.05-.14-.03-.21-.02-.09.02-1.49.95-4.22 2.79-.4.27-.76.41-1.08.4-.36-.01-1.04-.2-1.55-.37-.63-.2-1.12-.31-1.08-.66.02-.18.27-.36.74-.55 2.92-1.27 4.86-2.11 5.83-2.51 2.78-1.16 3.35-1.36 3.73-1.36.08 0 .27.02.39.12.1.08.13.19.14.27-.01.03.01.12 0 .28z"
-              />
-            </svg>
+            <TelegramIcon class="w-5 h-5" />
           </a>
           <a href="#" class="hover:text-white transition-colors">
-            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-              <path
-                d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 7.615a4.385 4.385 0 100 8.77 4.385 4.385 0 000-8.77zm0 7.234a2.849 2.849 0 110-5.698 2.849 2.849 0 010 5.698zm3.963-7.79a1.025 1.025 0 11-2.05 0 1.025 1.025 0 012.05 0z"
-              />
-            </svg>
+            <InstagramIcon class="w-5 h-5" />
+          </a>
+          <a href="#" class="hover:text-white transition-colors flex items-center gap-1">
+            <AvitoIcon class="w-6 h-6" />
+            <span class="block text-md font-bold">Avito</span>
           </a>
         </div>
 
--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import BaseButton from "~/components/UI/BaseButton.vue";
+import TelegramIcon from "~/assets/icons/telegram-icon.svg?component";
 </script>
 
 <template>
@@ -8,7 +9,7 @@ import BaseButton from "~/components/UI/BaseButton.vue";
     <div class="absolute inset-0 z-0 pointer-events-none"></div>
 
     <div class="relative z-10 grow flex flex-col justify-center max-w-7xl mx-auto px-6 w-full py-12">
-      <h1 class="text-white/70 text-lg tracking-[0.45em] uppercase mb-6 font-light">Премиальная оптика нового формата</h1>
+      <h1 class="text-white/70 text-lg tracking-[0.45em] uppercase mb-6 font-light">Оптика нового формата</h1>
 
       <h2 class="text-5xl md:text-[4rem] leading-tight uppercase mb-6 max-w-2xl font-light tracking-widest">
         Меняем линзы<br />
@@ -21,58 +22,47 @@ import BaseButton from "~/components/UI/BaseButton.vue";
       <p class="text-gray-300 text-lg leading-normal max-w-md mb-6">
         Изготавливаем линзы по вашему рецепту<br />
         для любых оправ — быстро, точно<br />
-        и дешевле сетевых оптик.
+        и дешевле сетевых оптик
       </p>
 
-      <div class="flex items-center flex-wrap gap-4 text-[0.85rem] text-gray-400 uppercase tracking-widest mb-12">
-        <span>Антиблик</span>
+      <div class="flex items-center flex-wrap gap-4 text-[0.85rem] text-white/60 uppercase tracking-widest mb-12">
+        <span>Прозрачные</span>
         <span class="text-[#c19d60] text-[8px]">●</span>
-        <span>Фотохром</span>
+        <span>Фотохромные</span>
         <span class="text-[#c19d60] text-[8px]">●</span>
-        <span>Астигматика</span>
-        <span class="text-[#c19d60] text-[8px]">●</span>
-        <span>Тонкие линзы</span>
+        <span>Тонированные</span>
       </div>
 
       <div class="flex items-center gap-10">
         <BaseButton>
           <template #icon>
-            <svg class="w-4 h-4 text-[#c19d60]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
-            </svg>
+            <TelegramIcon class="w-5 h-5 text-[#c19d60]" />
           </template>
-          Отправить рецепт
+          Отправить рецепт и фото оправы
         </BaseButton>
-
-        <a href="#" class="flex items-center gap-3 text-white text-[13px] uppercase tracking-widest hover:text-[#c19d60] duration-300">
-          Рассчитать стоимость
-          <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-          </svg>
-        </a>
       </div>
     </div>
 
     <ul class="relative z-10 w-full max-w-7xl mx-auto px-6 pb-12 mt-auto">
       <div class="flex items-start gap-12">
         <li class="flex flex-col border-l border-[#c19d60] pl-4">
-          <span class="text-gray-400 text-sm uppercase mb-1 tracking-wide">100%</span>
-          <span class="text-gray-500 text-[0.575rem] uppercase tracking-widest">Оригинальные линзы</span>
+          <span class="text-white/70 text-sm uppercase mb-1 tracking-wide">100%</span>
+          <span class="text-white/60 text-[0.575rem] uppercase tracking-widest">Оригинальные линзы</span>
         </li>
 
         <li class="flex flex-col border-l border-[#c19d60] pl-4">
-          <span class="text-gray-400 text-sm uppercase mb-1 mt-1 tracking-wide">Гарантия</span>
-          <span class="text-gray-500 text-[0.575rem] uppercase tracking-widest">На все работы</span>
+          <span class="text-white/70 text-sm uppercase mb-1 mt-1 tracking-wide">Гарантия</span>
+          <span class="text-white/60 text-[0.575rem] uppercase tracking-widest">На все работы</span>
         </li>
 
         <li class="flex flex-col border-l border-[#c19d60] pl-4">
-          <span class="text-gray-400 text-sm uppercase mb-1 mt-1 tracking-wide">Быстрое</span>
-          <span class="text-gray-500 text-[0.575rem] uppercase tracking-widest">Изготовление</span>
+          <span class="text-white/70 text-sm uppercase mb-1 mt-1 tracking-wide">Быстрое</span>
+          <span class="text-white/60 text-[0.575rem] uppercase tracking-widest">Изготовление</span>
         </li>
 
         <li class="flex flex-col border-l border-[#c19d60] pl-4">
-          <span class="text-gray-400 text-sm uppercase mb-1 mt-1 tracking-wide">Доставка</span>
-          <span class="text-gray-500 text-[0.575rem] uppercase tracking-widest">По всей России</span>
+          <span class="text-white/70 text-sm uppercase mb-1 mt-1 tracking-wide">Доставка</span>
+          <span class="text-white/60 text-[0.575rem] uppercase tracking-widest">По всей Москве и России</span>
         </li>
       </div>
     </ul>
