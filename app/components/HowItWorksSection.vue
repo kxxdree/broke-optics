4c1af980d65772fdbf811667d66cac87e106fb37
kxxdree <truebackward@gmail.com>
2026-09-20 13:24:53 +0300

    feat: add gallery

--- app/components/HowItWorksSection.vue
+++ app/components/HowItWorksSection.vue
@@ -1,31 +1,30 @@
 <script setup lang="ts">
 import BaseButton from "./UI/BaseButton.vue";
-import TelegramIcon from "~/assets/icons/telegram-icon.svg?component";
 
 const steps: { num: string; title: string; desc: string; image: string }[] = [
   {
     num: "01",
     title: "Отправляете рецепт и фото оправы",
     desc: "Любым удобным способом",
-    image: "/path-to-step-1.png",
+    image: "/assets/images/lens.jpg",
   },
   {
     num: "02",
     title: "Подбираем линзы и рассчитываем стоимость",
     desc: "Согласовываем с вами детали",
-    image: "/path-to-step-2.png",
+    image: "/assets/images/lens.jpg",
   },
   {
     num: "03",
     title: "Изготавливаем ваши линзы",
     desc: "Точно по рецепту на профессиональном оборудовании",
-    image: "/path-to-step-3.png",
+    image: "/assets/images/lens.jpg",
   },
   {
     num: "04",
     title: "Отправляем готовые очки",
     desc: "Надёжная упаковка и быстрая доставка",
-    image: "/path-to-step-4.png",
+    image: "/assets/images/lens.jpg",
   },
 ];
 </script>
@@ -55,10 +54,8 @@ const steps: { num: string; title: string; desc: string; image: string }[] = [
 
           <div class="w-8 h-px bg-[#c19d60]/50 mb-8 transition-all duration-300 group-hover:w-16 group-hover:bg-[#c19d60]"></div>
 
-          <div class="h-36 w-full mb-6 relative rounded flex items-center justify-start">
-            <!-- Заглушка. Когда появятся реальные картинки, замени этот div на:
-                  <img :src="step.image" alt="" class="max-h-full object-contain" />
-             -->
+          <div class="h-36 w-full mb-6 relative rounded flex">
+            <img :src="step.image" alt="" class="max-h-full w-full object-cover" />
           </div>
 
           <h3 class="text-white/80 text-[1rem] leading-snug mb-3 whitespace-pre-line">
